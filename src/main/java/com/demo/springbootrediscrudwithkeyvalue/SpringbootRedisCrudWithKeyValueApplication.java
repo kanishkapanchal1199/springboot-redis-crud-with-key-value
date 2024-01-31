01b3d81dd0d191f35b4f570b8d9f64dc3879ab80
Kanishka <kanishkajpanchal@outlook.com>
2024-01-31 18:31:47 +0530

    Added clear cache method in controller

--- src/main/java/com/demo/springbootrediscrudwithkeyvalue/SpringbootRedisCrudWithKeyValueApplication.java
+++ src/main/java/com/demo/springbootrediscrudwithkeyvalue/SpringbootRedisCrudWithKeyValueApplication.java
@@ -2,8 +2,10 @@ package com.demo.springbootrediscrudwithkeyvalue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringbootRedisCrudWithKeyValueApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/demo/springbootrediscrudwithkeyvalue/controller/ProductController.java
+++ src/main/java/com/demo/springbootrediscrudwithkeyvalue/controller/ProductController.java
@@ -3,19 +3,23 @@ package com.demo.springbootrediscrudwithkeyvalue.controller;
 import com.demo.springbootrediscrudwithkeyvalue.entity.Product;
 import com.demo.springbootrediscrudwithkeyvalue.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/product")
-@EnableCaching
+@CacheConfig(cacheNames={"Product"})
 public class ProductController {
     @Autowired
     private ProductService service;
+
+    @Autowired
+    private CacheManager cacheManager;
 
     @PostMapping
     public Product save(@RequestBody Product product) {
@@ -23,13 +27,12 @@ public class ProductController {
     }
 
     @GetMapping
-
     public List<Product> getAllProducts() {
         return service.getAllProducts();
     }
 
     @GetMapping("/{id}")
-    @Cacheable(key = "#id",value = "Product",unless = "#result.price>1000")
+    @Cacheable(key="#id",value = "Product",unless = "#result.price>1000")
     public Product findProductById(@PathVariable int id) {
         return service.findProduct(id);
     }
@@ -37,5 +40,25 @@ public class ProductController {
     @CacheEvict(key = "#id",value = "Product")
     public String remove(@PathVariable int id)   {
         return service.remove(id);
+    }
+
+        @DeleteMapping(path = "/clearCache")
+    public ResponseEntity<String> clearCache(){
+
+        if(cacheManager.getCacheNames().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No cache record has been found");
+        }else {
+
+
+            for(String name:cacheManager.getCacheNames()){
+                /* printing cache name */
+                System.out.println(name);
+
+                /* clear cache by name */
+                cacheManager.getCache(name).clear();
+
+            }
+            return ResponseEntity.status(HttpStatus.OK).body("All cache has been cleared");
+        }
     }
 }
